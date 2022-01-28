781969b2643848af60aab468ed2d3a19d2e12b64
Michael <michael.spengler@sap.com>
2022-01-28 10:40:33 +0100

    first commit

---
+++ src/rsa.spec.ts
@@ -0,0 +1,5 @@
+import { assertEquals } from "https://deno.land/std@0.86.0/testing/asserts.ts"
+
+Deno.test("should... ", async () => {
+    assertEquals(1, 1)
+})
---
+++ src/rsa.ts
@@ -0,0 +1,25 @@
+
+export class RSAService {
+
+    public hashContent(): string {
+        return "tbd"
+    }
+
+    public sign(): any {
+
+    }
+
+    public encrypt(): any {
+
+    }
+
+    public decrypt(): any {
+
+    }
+
+    public validateAuthenticity(): any {
+
+    }
+
+
+}
