1612694901b5249ce6e07db56be9e2717cc58809
Kruse, Jannis <jannis.kruse@dlr.de>
2022-01-29 11:20:42 +0100

    added basic rsa encryption/decryption and tests

--- src/rsa.spec.ts
+++ src/rsa.spec.ts
@@ -11,17 +11,46 @@ Deno.test("should return a valid RSA Key Pair ", async () => {
         fail(`I would have expected an RSA Key Pair`)
     }
 
+    console.log(newRSAKeyPair);
+
     // to be made more concrete
 })
 
 
-Deno.test("should...", async () => {
+Deno.test("should return the encrpyted message", async () => {
 
+    const rsaService = new RSAService()
+    const newRSAKeyPair = rsaService.generateKeyPair()
+
+    if (newRSAKeyPair === undefined) {
+        fail(`I would have expected an RSA Key Pair`)
+    }
+
+    const encryptedMessage = rsaService.encrypt(BigInt("9007199254740991"),newRSAKeyPair.privateKey)
+
+    if (newRSAKeyPair === undefined) {
+        fail(`I would have expected an encrypted message`)
+    }
+
+    console.log(encryptedMessage);
+    
 })
 
 
-Deno.test("should...", async () => {
+Deno.test("should return the decrypted message", async () => {
 
+    const rsaService = new RSAService()
+    const newRSAKeyPair = rsaService.generateKeyPair()
+
+    if (newRSAKeyPair === undefined) {
+        fail(`I would have expected an RSA Key Pair`)
+    }
+    const message = "9007199254740991"
+    const encryptedMessage = rsaService.encrypt(BigInt(message), newRSAKeyPair.privateKey)
+    const decrypedmessage = rsaService.decrypt(encryptedMessage, newRSAKeyPair.publicKey)
+
+    console.log(message)
+    console.log(decrypedmessage)
 })
 
 Deno.test("should...", async () => {
--- src/rsa.ts
+++ src/rsa.ts
@@ -1,16 +1,27 @@
+import * as B from 'https://deno.land/x/bigint/mod.ts'
 
+export interface key {
+    N: bigint
+    cryptionComponent: bigint
+}
 export interface KeyPair {
-    privateKey: string
-    publicKey: string
+    privateKey: key
+    publicKey: key
 }
 
 export class RSAService {
 
-    public generateKeyPair(): KeyPair {
-        return {
-            privateKey: "",
-            publicKey: ""
-        }
+
+    public generateKeyPair(keySize = 100) : KeyPair{
+        // two distinct prime numbers are chosen as random
+        let p = B.randomPrime(keySize)!, q=B.randomPrime(keySize)!
+
+        let N = p*q, r=(p-1n)*(q-1n)
+        let e = B.randomPrime(keySize)! // e<r
+        let d = B.modInv(e, r)! // ed=1 mod r
+
+        return {publicKey : {N : N,cryptionComponent : d},
+                privateKey: {N : N,cryptionComponent : e}}
     }
 
     public hashContent(): string {
@@ -21,12 +32,12 @@ export class RSAService {
 
     }
 
-    public encrypt(): any {
-
+    public encrypt(m:bigint, privateKey:key): any {
+        return B.modPow(m, privateKey.cryptionComponent, privateKey.N)
     }
 
-    public decrypt(): any {
-
+    public decrypt(c:bigint, publicKey:key): any {
+        return B.modPow(c, publicKey.cryptionComponent, publicKey.N)
     }
 
     public validateAuthenticity(): any {
